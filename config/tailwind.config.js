542bdb3795c94941d0b674a2a22a77e4eea6565b
lcrnjakovic <luka.crnjakovic@gmail.com>
2022-04-18 21:56:24 +0200

    Custom font and init navbar

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
+	  colors: {
+		  'white': '#F2F2F2',
+		  'red': '#D92525',
+		  'green': '#044040',
+		  'crimson_dark': '#591C21',
+		  'crimson_light': '#8C1F28'
+	  },
   },
   plugins: [
     require('@tailwindcss/forms'),
