4b9fbf94e364f016419c117425b5964a9d004b39
lcrnjakovic <luka.crnjakovic@gmail.com>
2022-11-17 19:41:11 +0100

    functioning db with models

--- luftinspektor.rb
+++ luftinspektor.rb
@@ -1,17 +1,20 @@
 require 'roda'
 require 'sequel'
+require 'pry'
 
 database = "luftinspektor_development"
-user     = ENV['PGUSER']
-password = ENV['PGPASSWORD']
+user     = 'lukacrnjakovic'
+password = ''
 DB = Sequel.connect(adapter: 'postgres', database: database, host: '127.0.0.1', user: user, password: password)
+
+Sequel::Model.plugin :timestamps, update_on_create: true
+
+require_relative 'models/post'
 
 class Luftinspektor < Roda
   plugin :public
   plugin :render
   plugin :head
-
-  Sequel::Model.plugin :timestamps, update_on_create: true
 
   route do |r|
     r.public
