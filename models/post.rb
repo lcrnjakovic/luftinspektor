852fbcd8b297640cf9819dc815a1f9e9298a36eb
lcrnjakovic <luka.crnjakovic@gmail.com>
2022-12-28 12:30:13 +0100

    added reading time

--- models/post.rb
+++ models/post.rb
@@ -2,10 +2,17 @@ class Post < Sequel::Model
   many_to_many :reactions, join_table: :post_reactions
   one_to_many :post_reactions
 
+  AVG_WPM = 238.0
+
   def after_create
     Reaction.all.each do |reaction|
       PostReaction.new(post_id: self.id, reaction_id: reaction.id).save
     end
     super
+  end
+
+  def reading_time
+    minutes = (content.split.count / AVG_WPM).round
+    "#{minutes} min"
   end
 end
